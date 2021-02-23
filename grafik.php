1c1e963816163786146b57cdc703de0208f06afd
Ismi Ana <79537614+izmian@users.noreply.github.com>
2021-02-24 04:42:52 +0700

    Update grafik.php

--- grafik.php
+++ grafik.php
@@ -118,7 +118,7 @@ $Kelurahan  = mysqli_query($koneksi, "SELECT kelurahan FROM wakaf order by id_wa
               <img class="img-fluid" src="img/268px-Kementerian_Agama_new_logo.png" width="70" height="70">
             </li>
           </ul>
-          <p class="copyright text-muted">Copyright &copy; SIGWAKAF 2020</p>
+          <p class="copyright text-muted">Copyright &copy; SIWAKAF 2020</p>
         </div>
       </div>
     </div>
